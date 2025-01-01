8d705f236a6783c6bfb0eb077ae8033b561bda35
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-02 00:14:49 +0700

    add footer and header

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+    <div class="bg-footer-custom text-white py-3">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center">
+                <i class="fas fa-tag me-2 footer-icon"></i>
+                <div>
+                    <strong>Flash Sale Setiap Hari</strong>
+                    <p class="footer-selling-dec mb-0">Dapatkan diskon menarik setiap hari</p>
+                </div>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fas fa-ranking-star me-2 footer-icon"></i>
+                <div>
+                    <strong>Barang Berkualitas Tinggi</strong>
+                    <p class="footer-selling-dec mb-0">Dapatkan barang dengan kualitas tinggi</p>
+                </div>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fas fa-headset me-2 footer-icon"></i>
+                <div>
+                    <strong>Customer Service 24 Jam</strong>
+                    <p class="footer-selling-dec mb-0">Kami siap menjawab keluhan Anda 24/7</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-black-custom text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 text-center text-md-start mb-4 mb-md-0">
+                    <img src="{{ asset('img/logo.svg') }}" alt="Logo" class="footer-logo rounded-circle" style="width: 100px;">
+                    <p class="mt-3" style="font-weight: bold">
+                        Dapatkan promo terbaru dan info lainnya di social media kami!
+                    </p>
+                    <div class="social-icons mt-3">
+                        <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-white me-2"><i class="fab fa-tiktok"></i></a>
+                        <a href="#" class="text-white me-2"><i class="fab fa-facebook"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-2 mb-4 mb-md-0">
+                    <h5 class="fw-bold">Bantuan</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">Syarat dan Ketentuan</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Alur Pemesanan Barang</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Pengembalian</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Kebijakan Privasi</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mb-4 mb-md-0">
+                    <h5 class="fw-bold">Sulvatara</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">Tentang Kami</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Lokasi Toko</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 text-center text-md-start">
+                    <div class="d-flex align-items-start">
+                        <i class="fas fa-headset me-2 footer-icon me-3"></i>
+                        <div>
+                            <h5 class="fw-bold mb-2">Layanan 24 Jam</h5>
+                            <p class="mb-1">cs@sulvatara-adv.com</p>
+                            <p class="mb-0"><i class="fab fa-whatsapp"></i><span class="ms-2">+62
+                                    822-2333-0774</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-footer-custom text-white text-center py-3">
+        <p class="mb-0">Copyright © 2025 Capstone Project Teknologi Informasi Universitas Muhammadiyah Yogyakarta. All
+            rights reserved.</p>
+    </div>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,44 @@
+<div class="bg-secondary text-white py-2" style="background-color: #222830 !important;">
+    <div class="container d-flex justify-content-between">
+        <a href="#" class="text-white me-3 text-decoration-none d-flex align-items-center">
+            <i class="fas fa-mobile-alt"></i>
+            <span class="ms-3">Download Sulvatara Adventure App</span>
+        </a>
+        <div>
+            <a href="#" class="text-white me-3 text-decoration-none">Tentang</a>
+            <a href="#" class="text-white text-decoration-none align-items-center">
+                <i class="fab fa-whatsapp"></i>
+                <span class="ms-1">Customer Service</span>
+            </a>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-black">
+    <div class="container">
+        <a href="" class="navbar-brand">
+            <img src="{{ asset('img/logo-white.svg') }}" alt="logo" style="width: 75px; height: 75px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain"
+            aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMain">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="#">Ransel</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tenda</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Sleeping Bag</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Jaket</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Sepatu</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Aksesoris</a></li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
