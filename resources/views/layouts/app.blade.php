e338e559139a433b58d95460f27336845c3dde89
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-14 13:54:37 +0700

    and mdsh and change class to wrapper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Sulvatara Adventure')</title>
+    <title>@yield('title') &mdash; Sulvatara Adventure</title>
     <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -19,7 +19,7 @@
 <body>
     <div id="app">
         @include('components.header')
-        <div class="container">
+        <div class="wrapper">
             @yield('main')
         </div>
         @include('components.footer')
