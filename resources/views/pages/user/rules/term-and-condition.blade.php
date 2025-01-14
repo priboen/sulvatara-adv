b53829c14929d0f32865b5faa40d509cec60f5d0
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-14 13:52:37 +0700

    create term pages

---
+++ resources/views/pages/user/rules/term-and-condition.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+@section('title', 'Syarat dan Ketentuan')
+@push('style')
+@endpush
+@section('main')
+    <div class="privacy-policy-header">
+        <div class="breadcumb-privacy text-center">
+            <p><a href="{{ route('home') }}" class="text-decoration-none">Beranda</a> / Syarat dan Ketentuan</p>
+            <h1>SYARAT DAN KETENTUAN</h1>
+        </div>
+    </div>
+    <div class="container pt-5" >
+        <div class="syarat-sewa">
+            <h1 class="title">TATA TERTIB PERSEWAAN</h1>
+            <ol>
+                <li>Penyewa Wajib mengambil barang sesuai dengan tanggal pemesanan dan mengembalikan barang sesuai dengan
+                    tanggal pengembalian.</li>
+                <li>Penyewa Wajib Mengecek semua perlengkapan yang disewa sebelum meninggalkan Lokasi persewaan Sulvatara
+                    Adventure. Kami tidak menerima komplain dalam bentuk apapun apabila Penyewa telah meninggalkan lokasi
+                    Persewaan.
+                </li>
+                <li>Peralatan/Perlengkapan persewaan dalam kondisi baik dan utuh, Penyewa Wajib merawat dan menjaganya
+                    dengan baik.</li>
+                <li>Jam Kerja kami adalah Pukul <strong>08.00 – 21.00 WIB. (ON TIME)</strong> Jika terjadi keterlambatan
+                    pengembalian, maka
+                    di anggap memperpanjang sewa (tidak ada toleransi waktu lagi, jam 21.00 tepat kami TUTUP).</li>
+                <li>Saat Ibadah Shalat, Toko <strong>TUTUP / ISTIRAHAT</strong> sebentar. Harap Semua Pelanggan Bisa
+                    Menyesuaikan dan
+                    Menghormati.</li>
+                <li>Khusus hari Jum’at <strong>pukul 11.30 – 13.00 WIB TUTUP untuk melaksanakan IBADAH JUM’AT</strong>.</li>
+                <li>Di luar jam kerja tidak bisa kami layani.</li>
+                <li>Telpon, SMS, BBM, WA hanya untuk menanyakan stok barang dan informasi barang. Boking atau Pemesanan atau
+                    Pengubahan Pesanan pada Nota harus langsung Ke Basecamp dan membawa Nota Asli.</li>
+                <li>Sulvatara Adventure bukanlah tempat penitipan barang. Pelanggan di larang menitipkan barang bawaan
+                    apapun.</li>
+                <li>Sulvatara Adventure Setiap Hari BUKA. Libur atau TUTUP Juga bisa terjadi Karena adanya peristiwa/agenda
+                    yang bersifat Darurat atau Penting*</li>
+                <li>Apabila terjadi kerusakan atau hilang terhadap peralatan yang di sewa, penyewa bertanggung jawab
+                    sepenuhnya untuk memperbaiki atau mengganti peralatan yang sama/serupa. (untuk keterangan denda dan
+                    kerusakan bisa dilihat di <strong>KITAB UNDANG-UNDANG PERSEWAAN KERUSAKAN/DENDA</strong>).</li>
+                <li>Persiapkanlah kegiatan anda dengan baik (Manajemen waktu, manajemen Perjalanan dan manajemen
+                    perlengkapan) karena kegiatan outdoor adalah kegiatan yang berisiko tinggi dan susah di prediksi.</li>
+                <li>Ketentuan tersebut di atas dapat berubah sesuai dengan situasi dan kondisi***.
+                </li>
+                <li>Point-point tersebut di atas merupakan kesepakatan yang harus ditaati bagi penyewa.
+                </li>
+            </ol>
+        </div>
+    </div>
+    <div class="container pt-5">
+        <div class="syarat-sewa">
+            <h1 class="title">SYARAT DAN KETENTUAN YANG BERLAKU</h1>
+            <ol>
+                <li>Perhitungan Sewa di SULVATARA ADVENTURE adalah Permalam**
+                </li>
+                <li>Saat pengambilan perlengkapan/barang, setiap penyewa wajib meninggalkan kartu identitas berupa :
+                    <strong>Kartu
+                        Pelajar / Kartu Identitas Anak (KIA) / KTM (minimal angkatan diatas 2014) / E-KTP / SIM</strong>,
+                    yang masih
+                    berlaku dan berdomisili di DIY. Untuk E-KTPJATENG bisa MENGGUNAKAN TAMBAHAN MATERAI 6.000. Atau bisa
+                    juga menggunakan jaminan uang senilai harga jual barang yang di sewa. Untuk E-KTP diLuar DIY DAN JATENG
+                    bisa dikenakan jaminan uang senilai 50% dari harga jual barang
+                </li>
+                <li>Pengagendaan dan Penjadwalan perlengkapan persewaan hanya diperuntukan bagi penyewa/pelanggan yang telah
+                    membayar Dana Pertama (DP).
+                </li>
+                <li>Besarnya Dana Pertama (DP) adalah Minimal 25% dari seluruh Dana Sewa. Untuk Pemesanan Partai Besar
+                    (Seluruh Dana Sewa diatas 3jt) Minimal DP 50%.
+                </li>
+                <li>Bawalah Nota anda saat Pengambilan maupun Pengembalian Perlengkapan / barang.
+                </li>
+                <li>Saat pengambilan perlengkapan/barang, setiap penyewa WAJIB Melunasi Seluruh Dana Sewa.
+                </li>
+                <li>Kami tidak melayani Pembatalan Pesanan, Pengurangan Jumlah Pesanan, Ganti Pesanan atau Cancel, maupun
+                    Ganti Hari Pengambilan setelah memasuki H-1 dan hari H dari jadwal pengambilan pada nota (walaupun sudah
+                    di DP). Pembatalan akan dikenakan cas sebesar 25% dari total dana Sewa.
+                </li>
+                <li>Barang Persewaan yang sudah dibawa keluar dari area Persewaan Sulvatara Adventure sudah dianggap sewa.
+                </li>
+                <li>Jauhkan barang dari api, kecuali kompor dan nisting.
+                </li>
+                <li>Dilarang Merokok dan Memasak di dalam Tenda.</li>
+                <li>Dilarang mendirikan tenda terlalu dekat api unggun.</li>
+                <li>Dilarang Mendirikan tenda di lahan yang berbatu dan terdapat benda tajam lainya (patahan ranting,
+                    pecahan batu tajam, dll).</li>
+                <li>Membawa barang dengan baik dan benar.</li>
+                <li>Ketentuan tersebut di atas dapat berubah sesuai dengan situasi dan kondisi***.
+                </li>
+                <li>Point-point tersebut di atas merupakan kesepakatan yang harus di taati bagi penyewa.
+                </li>
+            </ol>
+        </div>
+        <div class="syarat-sewa pt-5 pb-5">
+            <h1 class="title">NB :</h1>
+            <ul>
+                <li>* Situasi dan kondisi yang terjadi bersifat insidental atau agenda penting (kondangan/wedding, lelayu,
+                    penutupan akses jalan,dll).
+                </li>
+                <li>** Perlu Kami tekan kan bahwa perhitungan sewa adalah Permalam, bukan 24 Jam + 13 Jam. Namun,
+                    Pemaksimalan waktu 24 Jam + 13 Jam bisa di lakukan jika pelanggan melakukan Pengambil barang Pada jam
+                    08.00 tepat dan mengembalikan barang sebelum jam 21.00 hari berikutnya <strong>(inilah yang di maksud
+                        PERMALAM)</strong>.
+                </li>
+                <li>*** Situasi dan kondisi yang terjadi bersifat BENCANA NASIONAL (Gempa Bumi, Angin Putting Beliung,
+                    Gunung Meletus, Banjir Bandang,dll)
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
+@push('scripts')
+@endpush
