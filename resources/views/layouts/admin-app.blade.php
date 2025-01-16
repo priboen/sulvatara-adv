2aacc0eb80fb9a9f8585079d873e1ddcbf95d4bf
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-16 20:25:45 +0700

    add favicon

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') &mdash; Sulvatara Adventure</title>
+    <link rel="icon" href="{{ asset('img/logo-white.svg') }}" type="image/svg+xml">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
